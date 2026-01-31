a9bf281523bfb64c27c0e22464a6682eb8660372
Ganesh0031 <gingle0731@gmail.com>
2026-01-31 11:06:13 +0530

    next gretest element add

--- src/ArraysProblem/NextGreterElement.java
+++ src/ArraysProblem/NextGreterElement.java
@@ -1,5 +1,22 @@
 package ArraysProblem;
 
 public class NextGreterElement {
-
+    public static void main(String[] args) {
+        int []arr={12,8,60,37,2,49,16,28,21};
+        int n=arr.length;
+        arr[n-1]=-1;
+        int highValue=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>highValue){
+                 highValue=arr[i];
+                arr[i]=-1;
+            }
+            else{
+                arr[i]=highValue;
+            }
+        }
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
 }
